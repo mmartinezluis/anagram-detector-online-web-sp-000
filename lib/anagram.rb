baa3c441ef31f8ebda36cc4f83207e61c7a75180
Luis Martinez <luis_mmartinez@live.com>
2021-01-03 01:37:12 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -2,14 +2,14 @@ require "pry"
 # Your code goes here!
 class Anagram
 
-  attr_accessor :word :
+  attr_accessor :word :array
 
   def intialize(word)
     @word = word
   end
 
   def match
-    array
+    
     binding.pry
   end
 
